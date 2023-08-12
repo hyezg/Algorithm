a66be13b7f817dcd367e1edff04db9fe9c28b0df
hyejeong <112006114+hyezg@users.noreply.github.com>
2023-08-12 17:34:57 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.cc
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	int max = 0, i, n;
+	int a[9];
+	
+	for( i = 0; i < 9; i++ ) {
+		scanf("%d", &a[i]);
+		
+		if( max < a[i]) { 
+			max = a[i];
+			n = i;
+		}
+	}
+	printf("%d \n%d", max, n+1);
+	
+	return 0;
+}
+
