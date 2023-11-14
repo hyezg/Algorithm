37491a36214922c8e7b56c53feda8f90db85ab27
HyeJeong <112006114+hyezg@users.noreply.github.com>
2023-11-15 01:07:04 +0900

    Create ex02.java

---
+++ 프로그래머스/unrated/120819. 아이스 아메리카노/ex02.java
@@ -0,0 +1,9 @@
+//다른 사람 풀이
+class Solution {
+    public int[] solution(int money) {
+        int[] answer = {0, 0};
+        answer[0] = money/5500;
+        answer[1] = money%5500;
+        return answer;
+    }
+}
