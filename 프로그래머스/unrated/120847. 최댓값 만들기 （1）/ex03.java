0d8dac2f5e227224b6a33f0cc4b510139ced72d0
HyeJeong <112006114+hyezg@users.noreply.github.com>
2023-11-15 01:11:45 +0900

    Create ex03.java

---
+++ 프로그래머스/unrated/120847. 최댓값 만들기 （1）/ex03.java
@@ -0,0 +1,23 @@
+//시도
+class {
+  int answer = 0;
+        int max = 0;
+        int sec = 0;
+        
+        //max 찾기!
+        for(int i=0; i<numbers.length; i++)  {
+            if(numbers[i] > max)    {
+                max = numbers[i];
+            }
+        }
+        //sec 찾기
+        for(int i=0; i < numbers.length; i++)  {
+            if(numbers[i] < max && numbers[i] > sec)  {
+                sec = numbers[i];
+            }
+            else if(i == max.length){
+                  sec =numbers[i];
+            }
+        }
+        answer = max * sec;
+}
