6acdb53cf0eedd472ddf6c2c056843c6b84bc10a
HyeJeong <112006114+hyezg@users.noreply.github.com>
2024-03-12 09:40:08 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 59 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(n: Int): Int {
+        var input = n
+        var answer = 0
+       
+        while(input != 0){
+            answer += input%10
+            input /=10
+        }
+        return answer
+    }
+}
