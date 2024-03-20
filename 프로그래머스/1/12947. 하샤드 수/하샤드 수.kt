a809029da76a5f198287dde9847eac6b9c1cefd0
HyeJeong <112006114+hyezg@users.noreply.github.com>
2024-03-20 09:31:35 +0900

    [level 1] Title: 하샤드 수, Time: 24.00 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.kt
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.kt
@@ -1,12 +1,10 @@
 class Solution {
     fun solution(x: Int): Boolean {
-        var answer = true
-        var result = x.toString().map { it.toString().toInt()} .toIntArray()
-        if(x % result.sum() == 0){
-            answer = true
-        } else {
-            answer = false
-        }
-        return answer
+       
+        var sum = x.toString()
+                    .map { it.toString().toInt() } 
+                    .toIntArray()
+                    
+        return x % sum.sum() == 0
     }
 }
