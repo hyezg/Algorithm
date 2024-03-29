1735c6155d7548b9c10c4897e71f5ebea8c999ba
HyeJeong <112006114+hyezg@users.noreply.github.com>
2024-03-29 10:07:31 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 19.91 ms, Memory: 64.2 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.kt
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.kt
@@ -1,15 +1,14 @@
 class Solution {
     fun solution(arr: IntArray): IntArray {
         var min = arr.minOrNull()
-        var muar = arr.toMutableList()
+        var mutablelist = arr.toMutableList()
 
-        if (muar.contains(min)) {
-            muar.remove(min)
-        } else if (muar.size == 1 && muar[0] == 10) {
-            muar.add(-1)
+        if (mutablelist.contains(min)) {
+            mutablelist.remove(min)
+        } else if (mutablelist.size == 1 && mutablelist[0] == 10) {
+            mutablelist.add(-1)
         }
 
-
-        return muar.toIntArray()
+        return mutablelist.toIntArray()
     }
 }
